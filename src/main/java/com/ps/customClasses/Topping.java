16fbb3d68a06edff971522c0588e7ae0af10122c
linotmike <128604219+linotmike@users.noreply.github.com>
2024-11-10 01:41:39 -0800

    Implement Topping class with price calculation based on size and type using PriceCalculator

--- src/main/java/com/ps/customClasses/Topping.java
+++ src/main/java/com/ps/customClasses/Topping.java
@@ -1,17 +1,38 @@
 package com.ps.customClasses;
 
-public class Topping  implements Product{
+import com.ps.customClasses.enums.Size;
+import com.ps.customClasses.enums.ToppingType;
+
+public class Topping implements Product {
 
     private String name;
     private double price;
-    private String type;
+    private ToppingType toppingType;
 
+    public Topping(String name, double price, Size size, ToppingType toppingType) {
+        this.name = name;
+        this.price = PriceCalculator.getToppingPrice(size, toppingType);
+        this.toppingType = toppingType;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public ToppingType getToppingType() {
+        return toppingType;
+    }
+
+    public void setToppingType(ToppingType toppingType) {
+        this.toppingType = toppingType;
+    }
 
     @Override
     public double getPrice() {
-        return 0;
+        return price;
     }
 }
