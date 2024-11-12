dfe5b835c729a159c2780e92a5cde351fb59f5ad
linotmike <128604219+linotmike@users.noreply.github.com>
2024-11-12 12:15:50 -0800

    Added properties for drink class implementing Product interface

--- src/main/java/com/ps/customClasses/Drink.java
+++ src/main/java/com/ps/customClasses/Drink.java
@@ -1,4 +1,17 @@
 package com.ps.customClasses;
 
-public class Drink {
+import com.ps.customClasses.enums.Size;
+
+public class Drink implements Product{
+
+  private Size size;
+  private String type;
+  private String flavor;
+  private double price;
+
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
 }
