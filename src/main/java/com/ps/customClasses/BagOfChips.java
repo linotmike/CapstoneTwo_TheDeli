e221d9ef50e4d493295d7c8ba7e68e695b66304b
linotmike <128604219+linotmike@users.noreply.github.com>
2024-11-12 13:52:21 -0800

    Added properties, getters and setters to the bag of chips class

---
+++ src/main/java/com/ps/customClasses/BagOfChips.java
@@ -0,0 +1,24 @@
+package com.ps.customClasses;
+
+public class BagOfChips implements Product{
+
+    private String type;
+    private static final double price = 1.50;
+
+    public BagOfChips(String type){
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public double getPrice(){
+        return price;
+    }
+}
--- src/main/java/com/ps/customClasses/Chips.java
+++
@@ -1,4 +0,0 @@
-package com.ps.customClasses;
-
-public class Chips {
-}
