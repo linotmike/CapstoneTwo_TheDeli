5defbe251ad36d5d2da62660f62ec6c4f6139b1d
linotmike <128604219+linotmike@users.noreply.github.com>
2024-11-12 20:01:04 -0800

    Added properties, getters and setters to the order class

--- src/main/java/com/ps/customClasses/Order.java
+++ src/main/java/com/ps/customClasses/Order.java
@@ -1,7 +1,29 @@
 package com.ps.customClasses;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
+    private List<Product> products;
 
+    public Order(){
+        this.products = new ArrayList<>();
+    }
+
+    public Order(List<Product> products) {
+        if(products != null){
+            this.products = products;
+        } else {
+            this.products = new ArrayList<>();
+        }
+
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
